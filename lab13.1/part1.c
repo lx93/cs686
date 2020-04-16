4e940394e1f10bed48644eee12f9a9db43f13b8f
Xiang Li <xli163@g1207.cs.usfca.edu>
2020-04-16 00:00:27 -0700

    finished lab13.1

---
+++ lab13.1/part1.c
@@ -0,0 +1,10 @@
+int main ( void )
+{
+
+	int i , j = 0;
+	for (i = 0; i < 100; i ++){
+		j += 2;
+	}
+	printf (" The value of j is : %d \n " , j );
+	return 0;
+}
---
+++ lab13.1/part3.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+char * copyStr ( const char * s ) {
+	char * dst = ( char *) malloc ( sizeof ( char ) * strlen ( s ) + 1);
+	if ( dst != NULL ) {
+		for ( int i = 0; i < strlen (s) + 1; ++i ) {
+			*(dst + i ) = *( s + i);
+		}
+	}
+	return dst ;
+}
+
+int main ( void ) {
+	char str [] = " test ";
+	printf ("1. %s \n " , str );
+	char * str1 = copyStr (str);
+	printf ("2. %s \n " , str1);
+	free ( str1 );
+	return 0;
+}
