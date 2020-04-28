f03303b89d0b9357144651576d86ac516e7c56bc
Xiang Li <lx93@me.com>
2020-04-28 15:41:31 -0700

    add 14.1

---
+++ lab14.1/cmake-build-debug/CMakeFiles/lab141.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab141.dir/main.c.o"
+  "lab141"
+  "lab141.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab141.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab14.1/main.c
@@ -0,0 +1,210 @@
+#define _XOPEN_SOURCE
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <crypt.h>
+
+
+struct user {
+    char username [50];
+    char password [256];
+    char firstname [50];
+    char lastname [50];
+    int admin ;
+};
+
+
+char * cs521Hash ( const char * password ) {
+     return crypt(password,"00");
+}
+
+struct user * createUsers ( int * count ) {
+    FILE *fp;
+    fp = fopen("/home/lx93/lab141/credential_file", "r");
+    if (fp==NULL) {
+        printf("Error in opening file.\n");
+        fclose(fp);
+        return NULL;
+    }
+
+    int c;
+    while ((c=getc(fp))!=EOF){
+        if (c=='\n'){
+            *count+=1;
+        }
+    }
+    *count+=1;
+    printf("%d",*count);
+    struct user* users = calloc(*count, sizeof(struct user));
+    fclose(fp);
+    return users;
+}
+
+void populateUsers ( void * users ) {
+    FILE* fp;
+    fp = fopen("/home/lx93/lab141/credential_file", "r");
+    if (fp==NULL) {
+        printf("Error in opening file.\n");
+        fclose(fp);
+        return;
+    }
+
+    char c;
+    int i = 0;
+    char* cp;
+    while (c!=EOF)
+    {
+        c = getc(fp);
+        cp = ((struct user*)users + i)->firstname;
+        while(c!='\t'){
+            *cp=c;
+            cp++;
+            c = getc(fp);
+        }
+        c = getc(fp);
+        cp = ((struct user*)users + i)->lastname;
+        while(c!='\t'){
+            *cp=c;
+            cp++;
+            c = getc(fp);
+        }
+        c = getc(fp);
+        cp = ((struct user*)users + i)->username;
+        while(c!='\t'){
+            *cp=c;
+            cp++;
+            c = getc(fp);
+        }
+        c = getc(fp);
+        cp = ((struct user*)users + i)->password;
+//        cp = cs521Hash(cp);
+        while(c!='\t'){
+            *cp=c;
+            cp++;
+            c = getc(fp);
+        }
+        c = getc(fp);
+        ((struct user*)users + i)->admin = c-48;
+
+        c = getc(fp);
+        i++;
+    }
+
+
+    fclose(fp);
+
+}
+
+int checkAdminPassword ( char * password , struct user * users , int count )
+{
+    for ( int i = 0; i < count ; ++ i ) {
+        if ( strcmp (( users + i ) -> username , "admin") == 0)
+        {
+            printf("%s\n",(users+i)->firstname);
+            printf("%s\n",password);
+            //#1Pa5
+            if (strcmp("s",password)==0) {
+                printf("password correct!\n");
+                return 1;
+            }
+            else {
+                printf("password incorrect!\n");
+
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
+
+
+struct user * addUser ( struct user * users , int * count , char * username , char * password , char * firstname , char * lastname , int admin ) {
+    *count+=1;
+    realloc(users, *count * sizeof(struct user));
+    strcpy((users+*count-1)->username,username);
+    strcpy((users+*count-1)->password,password);
+    strcpy((users+*count-1)->firstname,firstname);
+    strcpy((users+*count-1)->lastname,lastname);
+    (users+*count-1)->admin = admin;
+    return users;
+}
+
+
+
+void saveUsers ( struct user * users , int count ) {
+    FILE* fp;
+    fp = fopen("/home/lx93/lab141/credential_file2", "w");
+    if (fp==NULL) {
+        printf("Error in opening file.\n");
+        fclose(fp);
+        return;
+    }
+
+    char* cp;
+    for (int i=0;i<count;i++){
+        cp = (users + i)->firstname;
+        while (*cp!=0){
+            putc(*cp,fp);
+            cp++;
+        }
+        putc('\t',fp);
+
+        cp = (users + i)->lastname;
+        while (*cp!=0){
+            putc(*cp,fp);
+            cp++;
+        }
+        putc('\t',fp);
+
+        cp = (users + i)->username;
+        while (*cp!=0){
+            putc(*cp,fp);
+            cp++;
+        }
+        putc('\t',fp);
+
+        cp = (users + i)->password;
+        while (*cp!=0){
+            putc(*cp,fp);
+            cp++;
+        }
+        putc('\t',fp);
+        putc((users + i)-> admin+48,fp);
+        putc('\n',fp);
+    }
+
+    fclose(fp);
+}
+
+
+int main ( void )
+{
+    int user_count = 0;
+    struct user *users = createUsers(&user_count);
+    if ( users == NULL ) return EXIT_FAILURE ;
+
+    populateUsers (users);
+    printf (" Enter admin password to add new users :");
+    char entered_admin_password [50];
+    scanf ("%s" , entered_admin_password );
+    if ( checkAdminPassword ( entered_admin_password , users , user_count ))
+    {
+        struct user new_user ;
+        printf (" Enter username :");
+        scanf ("%s" , new_user.username );
+        printf (" Enter first name :");
+        scanf ("%s" , new_user.firstname );
+        printf (" Enter last name :");
+        scanf ("%s" , new_user.lastname );
+        printf (" Enter password :");
+        scanf ("%s" , new_user.password );
+        printf (" Enter admin level :");
+        scanf ("%d" , &(new_user.admin));
+        users = addUser ( users , &user_count , new_user.username , new_user.password , new_user.firstname , new_user.lastname , new_user.admin );
+        if ( users == NULL ) {return EXIT_FAILURE;}
+    }
+    saveUsers ( users , user_count );
+    free ( users );
+    return EXIT_SUCCESS ;
+}
